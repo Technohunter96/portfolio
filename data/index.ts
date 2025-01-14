7f9baaa7220ee9d736094bca5e7e6cbd1b47c35e
Michal <michalhavlik2@gmail.com>
2025-01-14 14:42:48 +0100

    Update of InsideScoop

--- data/index.ts
+++ data/index.ts
@@ -48,7 +48,7 @@ export const gridItems = [
   },
   {
     id: 5,
-    title: 'Currently learning and getting better in Next.js, PowerShell & SQL',
+    title: 'Building an E-Commerce Platform with Next.js, React, TypeScript, and Prisma',
     description: 'The Inside Scoop',
     className: 'md:col-span-3 md:row-span-2',
     imgClassName: 'absolute right-0 bottom-0 md:w-96 w-60',
