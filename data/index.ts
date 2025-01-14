e651f917e9e8e058698122087d51e75030781d69
Michal <michalhavlik2@gmail.com>
2025-01-14 14:36:38 +0100

    icons

--- data/index.ts
+++ data/index.ts
@@ -1,199 +1,215 @@
 export const navItems = [
-    { name: "About", link: "#about" },
-    { name: "Projects", link: "#projects" },
-    { name: "Experience", link: "#experience" },
-    { name: "Contact", link: "#contact" },
-  ];
-  
-  export const gridItems = [
-    {
-      id: 1,
-      title: "I prioritize client collaboration, fostering open communication ",
-      description: "",
-      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
-      imgClassName: "w-full h-full",
-      titleClassName: "justify-end",
-      img: "/b1.svg",
-      spareImg: "",
-    },
-    {
-      id: 2,
-      title: "I'm very flexible with time zone communications",
-      description: "",
-      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
-      imgClassName: "",
-      titleClassName: "justify-start",
-      img: "",
-      spareImg: "",
-    },
-    {
-      id: 3,
-      title: "My tech stack",
-      description: "I constantly try to improve",
-      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
-      imgClassName: "",
-      titleClassName: "justify-center",
-      img: "",
-      spareImg: "",
-    },
-    {
-      id: 4,
-      title: "Tech enthusiast with a passion for development.",
-      description: "",
-      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
-      imgClassName: "",
-      titleClassName: "justify-start",
-      img: "/grid.svg",
-      spareImg: "/b4.svg",
-    },
-    {
-      id: 5,
-      title: "Currently learning and getting better in Next.js, PowerShell & SQL",
-      description: "The Inside Scoop",
-      className: "md:col-span-3 md:row-span-2",
-      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
-      titleClassName: "justify-center md:justify-start lg:justify-center",
-      img: "/b5.svg",
-      spareImg: "/grid.svg",
-    },
-    {
-      id: 6,
-      title: "Do you want to start a project together?",
-      description: "",
-      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
-      imgClassName: "",
-      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
-      img: "",
-      spareImg: "",
-    },
-  ];
-  
-  export const projects = [
-    {
-      id: 1,
-      title: "Property Pulse",
-      des: "Next.js app for listing and searching rental properties.",
-      img: "/propertyPulse.svg",
-      iconLists: ["/re.svg", "/cloud.svg", "/next.svg", "/tail.svg", "/mongo.svg", "/mapbox.svg"],
-      link: "https://github.com/Technohunter96/property-pulse",
-      url: "https://property-pulse-teal.vercel.app/",
-    },
-    {
-      id: 2,
-      title: "BookStore",
-      des: "Book database platform built with the MERN stack & Redux.",
-      img: "/book.svg",
-      iconLists: ["/re.svg", "/bs.svg", "/mongo.svg", "/redux.svg", "/node-js.svg"],
-      link: "https://github.com/Technohunter96/BookStore",
-      url: "https://bookstore-ls6g.onrender.com/",
-    },
-    {
-      id: 3,
-      title: "House Marketplace",
-      des: "Marketplace for houses build with React & Firebase v9.",
-      img: "/house.svg",
-      iconLists: ["/re.svg", "/firebase.svg", "/leaflet.svg", "/google.svg"],
-      link: "https://github.com/Technohunter96/house-marketplace",
-      url: "https://house-marketplace-lemon-two.vercel.app/",
-    },
-    {
-      id: 4,
-      title: "Animated Apple Iphone 3D Website",
-      des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-      img: "/p4.svg",
-      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-      link: "https://github.com/Technohunter96/apple_website",
-      url: "https://apple-website-lilac-two.vercel.app/",
-    },
-    {
-      id: 5,
-      title: "Support Desk App",
-      des: "Support ticket app built with the MERN stack",
-      img: "/support.svg",
-      iconLists: ["/re.svg", "/bs.svg", "/mongo.svg", "/redux.svg", "/node-js.svg"],
-      link: "https://github.com/Technohunter96/support-desk-app",
-      url: "https://support-desk-multihunter.onrender.com/",
-    },
-    {
-      id: 6,
-      title: "Developer portfolio",
-      des: "Minimalist Dev Portfolio with 3D elements & beautiful animations ",
-      img: "/portfolio.svg",
-      iconLists: ["/re.svg", "/next.svg", "/tail.svg", "/three.svg", "/framer.svg", "sentry.svg"],
-      link: "https://github.com/Technohunter96/portfolio",
-      url: "/",
-    },
-  ];
+  { name: 'About', link: '#about' },
+  { name: 'Projects', link: '#projects' },
+  { name: 'Experience', link: '#experience' },
+  { name: 'Contact', link: '#contact' },
+];
 
-  
-  export const companies = [
-    {
-      id: 1,
-      name: "JavaScript",
-      img: "/js.svg",
-      nameImg: "/cloudName.svg",
-    },
-    {
-      id: 2,
-      name: "React",
-      img: "/re.svg",
-      nameImg: "/appName.svg",
-    },
-    {
-      id: 3,
-      name: "Redux",
-      img: "/redux.svg",
-      nameImg: "/dockerName.svg",
-    },
-    
-    {
-      id: 5,
-      name: "Sharepoint",
-      img: "/sharepoint.svg",
-      nameImg: "/hostName.svg",
-    },
-    {
-      id: 6,
-      name: "Power Automate",
-      img: "/pa.svg",
-      nameImg: "/streamName.svg",
-    },
-    {
-      id: 7,
-      name: "Microsoft",
-      img: "/ms.svg",
-      nameImg: "/streamName.svg",
-    },
-    
-  ];
-  
-  export const workExperience = [
-    {
-      id: 1,
-      title: "Artillery Reconnaissance",
-      desc: "Served as a reconnaissance specialist, gaining valuable experience in teamwork, discipline, precision, and attention to detail.",
-      className: "md:col-span-2",
-      thumbnail: "/soldier.svg",
-    },
-    {
-      id: 2,
-      title: "SharePoint Developer",
-      desc: "Designed and implemented intranet applications using Nintex Forms & Flows, Plumsail Forms, and Power Automate. Managed data integrations with SQL and PowerShell for seamless workflows.",
-      className: "md:col-span-2",
-      thumbnail: "/exp4.svg",
-    },
-  ];
-  
-  
-  export const socialMedia = [
-    {
-      id: 1,
-      img: "/git.svg",
-      url: "https://github.com/Technohunter96",
-    },
-    {
-      id: 3,
-      img: "/link.svg",
-      url: "https://www.linkedin.com/in/michal-havl%C3%ADk-35068a290/",
-    },
-  ];
+export const gridItems = [
+  {
+    id: 1,
+    title: 'I prioritize client collaboration, fostering open communication ',
+    description: '',
+    className: 'lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]',
+    imgClassName: 'w-full h-full',
+    titleClassName: 'justify-end',
+    img: '/b1.svg',
+    spareImg: '',
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: '',
+    className: 'lg:col-span-2 md:col-span-3 md:row-span-2',
+    imgClassName: '',
+    titleClassName: 'justify-start',
+    img: '',
+    spareImg: '',
+  },
+  {
+    id: 3,
+    title: 'My tech stack',
+    description: 'I constantly try to improve',
+    className: 'lg:col-span-2 md:col-span-3 md:row-span-2',
+    imgClassName: '',
+    titleClassName: 'justify-center',
+    img: '',
+    spareImg: '',
+  },
+  {
+    id: 4,
+    title: 'Tech enthusiast with a passion for development.',
+    description: '',
+    className: 'lg:col-span-2 md:col-span-3 md:row-span-1',
+    imgClassName: '',
+    titleClassName: 'justify-start',
+    img: '/grid.svg',
+    spareImg: '/b4.svg',
+  },
+  {
+    id: 5,
+    title: 'Currently learning and getting better in Next.js, PowerShell & SQL',
+    description: 'The Inside Scoop',
+    className: 'md:col-span-3 md:row-span-2',
+    imgClassName: 'absolute right-0 bottom-0 md:w-96 w-60',
+    titleClassName: 'justify-center md:justify-start lg:justify-center',
+    img: '/b5.svg',
+    spareImg: '/grid.svg',
+  },
+  {
+    id: 6,
+    title: 'Do you want to start a project together?',
+    description: '',
+    className: 'lg:col-span-2 md:col-span-3 md:row-span-1',
+    imgClassName: '',
+    titleClassName: 'justify-center md:max-w-full max-w-60 text-center',
+    img: '',
+    spareImg: '',
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: 'Property Pulse',
+    des: 'Next.js app for listing and searching rental properties.',
+    img: '/propertyPulse.svg',
+    iconLists: [
+      '/re.svg',
+      '/cloud.svg',
+      '/next.svg',
+      '/tail.svg',
+      '/mongo.svg',
+      '/mapbox.svg',
+    ],
+    link: 'https://github.com/Technohunter96/property-pulse',
+    url: 'https://property-pulse-teal.vercel.app/',
+  },
+  {
+    id: 2,
+    title: 'BookStore',
+    des: 'Book database platform built with the MERN stack & Redux.',
+    img: '/book.svg',
+    iconLists: ['/re.svg', '/bs.svg', '/mongo.svg', '/redux.svg', '/node-js.svg'],
+    link: 'https://github.com/Technohunter96/BookStore',
+    url: 'https://bookstore-ls6g.onrender.com/',
+  },
+  {
+    id: 3,
+    title: 'House Marketplace',
+    des: 'Marketplace for houses build with React & Firebase v9.',
+    img: '/house.svg',
+    iconLists: ['/re.svg', '/firebase.svg', '/leaflet.svg', '/google.svg'],
+    link: 'https://github.com/Technohunter96/house-marketplace',
+    url: 'https://house-marketplace-lemon-two.vercel.app/',
+  },
+  {
+    id: 4,
+    title: 'Animated Apple Iphone 3D Website',
+    des: 'Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..',
+    img: '/p4.svg',
+    iconLists: ['/next.svg', '/tail.svg', '/ts.svg', '/three.svg', '/gsap.svg'],
+    link: 'https://github.com/Technohunter96/apple_website',
+    url: 'https://apple-website-lilac-two.vercel.app/',
+  },
+  {
+    id: 5,
+    title: 'Support Desk App',
+    des: 'Support ticket app built with the MERN stack',
+    img: '/support.svg',
+    iconLists: ['/re.svg', '/bs.svg', '/mongo.svg', '/redux.svg', '/node-js.svg'],
+    link: 'https://github.com/Technohunter96/support-desk-app',
+    url: 'https://support-desk-multihunter.onrender.com/',
+  },
+  {
+    id: 6,
+    title: 'Developer portfolio',
+    des: 'Minimalist Dev Portfolio with 3D elements & beautiful animations ',
+    img: '/portfolio.svg',
+    iconLists: [
+      '/re.svg',
+      '/next.svg',
+      '/tail.svg',
+      '/three.svg',
+      '/framer.svg',
+      'sentry.svg',
+    ],
+    link: 'https://github.com/Technohunter96/portfolio',
+    url: '/',
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: 'JavaScript',
+    img: '/js.svg',
+    nameImg: '/cloudName.svg',
+  },
+  {
+    id: 2,
+    name: 'React',
+    img: '/re.svg',
+    nameImg: '/appName.svg',
+  },
+  {
+    id: 3,
+    name: 'Redux',
+    img: '/redux.svg',
+    nameImg: '/dockerName.svg',
+  },
+  {
+    id: 4,
+    name: 'Next.js',
+    img: '/nextjs.svg',
+    nameImg: '/dockerName.svg',
+  },
+  {
+    id: 5,
+    name: 'Sharepoint',
+    img: '/sharepoint.svg',
+    nameImg: '/hostName.svg',
+  },
+  {
+    id: 6,
+    name: 'Power Automate',
+    img: '/pa.svg',
+    nameImg: '/streamName.svg',
+  },
+  {
+    id: 7,
+    name: 'Microsoft',
+    img: '/ms.svg',
+    nameImg: '/streamName.svg',
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: 'Junior Front-end Developer',
+    desc: 'Developed dynamic UI components using React and Tailwind CSS. Focused on responsive design, user experience, and performance optimization.',
+    className: 'md:col-span-2',
+    thumbnail: '/exp4.svg',
+  },
+  {
+    id: 2,
+    title: 'SharePoint Developer',
+    desc: 'Designed and implemented intranet applications using Nintex Forms & Flows, Plumsail Forms, and Power Automate. Managed data integrations with SQL and PowerShell for seamless workflows.',
+    className: 'md:col-span-2',
+    thumbnail: '/exp2.svg',
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: '/git.svg',
+    url: 'https://github.com/Technohunter96',
+  },
+  {
+    id: 3,
+    img: '/link.svg',
+    url: 'https://www.linkedin.com/in/michal-havl%C3%ADk-35068a290/',
+  },
+];
