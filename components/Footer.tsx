bd9b6e100bcc9f36516b8dc9c1de85bb4236d10c
Michal <michalhavlik2@gmail.com>
2025-01-14 14:56:55 +0100

    Footer Gap

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,49 +1,51 @@
-"use client";
+'use client';
 
-import { FaLocationArrow } from "react-icons/fa"
-import MagicButton from "./MagicButton"
-import { socialMedia } from "@/data"
+import { FaLocationArrow } from 'react-icons/fa';
+import MagicButton from './MagicButton';
+import { socialMedia } from '@/data';
 
 const Footer = () => {
   return (
-    <footer className="w-full pb-10 mb-[100px] md:mb-5" id="contact">
-        <div className="flex flex-col items-center">
-            <h1 className="heading lg:max-w-[45vw]">
-                Ready to take <span className="text-purple">your</span> project to the <span className="text-purple">next</span> level?
-            </h1>
-            <p className="text-white-200 md:mt-10 my-5 text-center">
-                Reach out to me today and let&apos;s discuss how I can help you achieve your goals. 
-            </p>
-            <a href="mailto:michalhavlik2@gmail.com">
-                <MagicButton 
-                    title="Let's get in touch"
-                    icon={<FaLocationArrow/>}
-                    position="right"
-                    otherClasses="hover:bg-slate-900"
-                />
+    <footer className='w-full pb-10 mb-[100px] md:mb-5' id='contact'>
+      <div className='flex flex-col items-center'>
+        <h1 className='heading lg:max-w-[45vw]'>
+          Ready to take <span className='text-purple'>your</span> project to the{' '}
+          <span className='text-purple'>next</span> level?
+        </h1>
+        <p className='text-white-200 md:mt-10 my-5 text-center'>
+          Reach out to me today and let&apos;s discuss how I can help you achieve your
+          goals.
+        </p>
+        <a href='mailto:michalhavlik2@gmail.com'>
+          <MagicButton
+            title="Let's get in touch"
+            icon={<FaLocationArrow />}
+            position='right'
+            otherClasses='hover:bg-slate-900'
+          />
+        </a>
+      </div>
+
+      <div className='flex mt-16 md:flex-row flex-col justify-between items-center gap-6'>
+        <p className='md:text-base text-sm md:font-normal font-light'>
+          Copyright © 2024 Michal Havlik
+        </p>
+
+        <div className='flex items-center md:gap-3 gap-6'>
+          {socialMedia.map((info) => (
+            <a
+              key={info.id}
+              href={info.url}
+              target='_blank'
+              rel='noopener noreferrer'
+              className='w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300 hover:bg-slate-800'
+            >
+              <img src={info.img} alt={`social-icon-${info.id}`} width={20} height={20} />
             </a>
+          ))}
         </div>
-
-        <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
-            <p className="md:text-base text-sm md:font-normal font-light">
-                Copyright © 2024 Michal Havlik
-            </p>
-
-            <div className="flex items-center md:gap-3 gap-6">
-                {socialMedia.map((info) => (
-                    <a
-                      key={info.id}
-                      href={info.url}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300 hover:bg-slate-800"
-                    >
-                      <img src={info.img} alt={`social-icon-${info.id}`} width={20} height={20} />
-                    </a>
-                ))}
-            </div>
-        </div>
+      </div>
     </footer>
-  )
-}
-export default Footer
+  );
+};
+export default Footer;
