c3bff6e156ad5666acb72e33234fbb92345e783a
Michal <michalhavlik2@gmail.com>
2025-01-25 10:34:31 +0100

    GA

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { ThemeProvider } from './provider';
+import Script from 'next/script';
 
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { ThemeProvider } from "./provider"
-
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: "Michal's Portfolio",
-  description: "Modern & Minimal Portfolio",
+  description: 'Modern & Minimal Portfolio',
 };
 
 export default function RootLayout({
@@ -17,18 +17,33 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang='en' suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/favicone.svg" sizes="any" />
+        <link rel='icon' href='/favicone.svg' sizes='any' />
       </head>
       <body className={inter.className}>
         <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
+          attribute='class'
+          defaultTheme='dark'
           enableSystem
           disableTransitionOnChange
         >
           {children}
+          <Script
+            id='google-analytics'
+            strategy='afterInteractive'
+            dangerouslySetInnerHTML={{
+              __html: `
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-BKRKW9YDV0"></script>
+                <script>
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', 'G-BKRKW9YDV0');
+                </script>
+              `,
+            }}
+          />
         </ThemeProvider>
       </body>
     </html>
